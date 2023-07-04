a1cc9547d5d2302c109378f5eb40b30edc1aa373
WilliansMercante <willians_mercante@hotmail.com>
2023-07-04 14:41:08 -0300

    Subindo arquivos exemplo

---
+++ TESTE/Controllers/ArquivoController.cs
@@ -0,0 +1,108 @@
+﻿using Dapper.Contrib.Extensions;
+
+using Microsoft.AspNetCore.Mvc;
+
+using MySqlConnector;
+
+using TESTE.Models;
+
+namespace TESTE.Controllers
+{
+    public class ArquivoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Gravar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LerArquivo(IFormFile file)
+        {
+
+            try
+            {
+                var nome = file.FileName.Trim();
+                var leitura = file.OpenReadStream();
+                using (var reader = new StreamReader(leitura))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string linha = reader.ReadLine();
+
+                        conteudo_arquivo conteudo_arquivo = new conteudo_arquivo() { linha = linha, nm_arquivo = nome, dt_cadastro = DateTime.Now };
+                        InserirLinha(conteudo_arquivo);
+                    }
+
+                }
+
+                Console.WriteLine("teste");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Erro: " + e.Message);
+            }
+
+            return View("Index");
+        }
+
+        public void InserirLinha(conteudo_arquivo conteudo_arquivo)
+        {
+            try
+            {
+
+                string connectionString = "Server=localhost; port=3306; User ID=root; Password=123456; Database=arquivo";
+
+
+
+                using (var db = new MySqlConnection(connectionString))
+                {
+                    db.Open();
+
+                    db.Insert(conteudo_arquivo);
+
+                    db.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Erro: " + e.Message);
+            }
+        }
+
+        [HttpPost]
+        public void GravarArquivo(IFormFile file)
+        {
+            string caminho = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\arquivos\\teste");
+
+            using (FileStream fs = System.IO.File.Create(Path.Combine(caminho, file.FileName)))
+            {
+                file.CopyTo(fs);
+            }
+        }
+
+
+
+        //comandos
+
+
+        //            var query = "Select * from conteudo_arquivo";
+        //            var clientes = db.Query<conteudo_arquivo>(query);
+
+        //var dados = db.GetAll<conteudo_arquivo>();
+
+
+
+
+
+
+
+    }
+}
---
+++ TESTE/Models/conteudo_arquivo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+//using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TESTE.Models
+{
+    [Table("conteudo_arquivo")]
+    public class conteudo_arquivo
+    {
+        public int id_conteudo_arquivo { get; set; }
+        public string linha { get; set; }
+        public string nm_arquivo { get; set; }
+        public DateTime dt_cadastro { get; set; }
+    }
+}
